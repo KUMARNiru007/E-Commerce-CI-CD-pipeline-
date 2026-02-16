0f84756457e65941048439995df0c0d16909ad58
Kumar Nirupam <kumar.nirupam24@gmail.com>
2026-02-16 19:23:16 +0530

    auth done

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import {
+    registerUser , 
+    verifyUser ,
+     login ,getMe ,
+      logoutUser } from "../controllers/auth.controllers.js"
+import {isLoggedIn} from "../middleware/auth.middleware.js"
+
+const router = express.Router()
+
+router.post("/register",registerUser)
+router.get("/verify/:token",verifyUser)
+router.post("/login",login)
+router.get("/me",isLoggedIn,getMe)
+router.get("/logout",isLoggedIn,logoutUser)
+
+export default router;
---
+++ backend/utils/db.connect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
+
+const db =()=>{
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        console.log("connected to mongodb");
+    })
+    .catch(()=>{
+        "Error connecting mongodb"
+    })
+}
+
+export default db;
